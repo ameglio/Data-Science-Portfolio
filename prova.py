28afa84781da2bd4c51c7ad2bee35eefe114cb56
Davide Ameglio <ameglio.business@gmail.com>
2020-03-19 17:24:26 +0100

    Ciao

---
+++ prova.py
@@ -0,0 +1,14 @@
+#Convert to kelvin
+gradi = float(input('Inserisci temperatura in °C: '))
+kelvin = gradi + 215.15
+print(kelvin)
+
+
+#Print input
+Nome = input('Inserisci nome: ')
+Indirizzo = input('Inserisci indirizzo: ')
+cap = input('cap: ')
+stato = input('Stato: ')
+
+print('Il nome è...',Nome)
+
